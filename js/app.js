893c9f8def87da5c2b057e905a760eeec25f1bc9
thedegar <tchedegard@gmail.com>
2014-05-15 22:33:47 -0500

    Animation + Sound
    Added animation to Hadouken and sound.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+	function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
+
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -8,9 +14,18 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
+			.animate(
+				{'left': '300px'},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left', '-180px');
+				}
+			);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
